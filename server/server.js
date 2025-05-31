cf0243f3186af5d64ce5cd241992936196628235
Your Namesanthosh <you@example.com>
2025-05-31 16:31:11 +0530

    "done"

--- server/server.js
+++ server/server.js
@@ -22,10 +22,21 @@ connectToDb();
 
 
 app.use(express.json());
-app.use(cors());
+const corsOptions = {
+  origin: [
+    `${process.env.FRONTEND_URL}`,  
+    'http://localhost:3000'                                
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 
-// Handle preflight requests
-app.options('*', cors());
+ 
+};
+
+
+app.use(cors(corsOptions));
+
+
+app.options('*', cors(corsOptions));
 
 app.get('/',(req,res)=>{
   res.send("api working")
