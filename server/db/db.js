1f4c0d4f82f68278f8754941653782a1d46efdad
Your Namesanthosh <you@example.com>
2025-05-31 12:48:41 +0530

    "done"

--- server/db/db.js
+++ server/db/db.js
@@ -1,23 +1,14 @@
-// import mongoose from "mongoose";
 
-// const connectToDb = () => {
-//     mongoose.connect(process.env.MONGO_URI).then(() => {
-//         console.log("Connected to database");
-//     }).catch((err) => {
-//         console.log("Error while connecting database:", err);
-//     })
-// }
-
-// export default connectToDb;
 
 
 import mongoose from "mongoose";
 
 const connectToDb = () => {
- mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+  mongoose
+    .connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
     .then(() => {
       console.log("Connected to database");
     })
