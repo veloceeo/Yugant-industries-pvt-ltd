4d8d330e71c63becf1f7889553b3f08e283136c4
Your Namesanthosh <you@example.com>
2025-05-31 12:44:45 +0530

    "client"

--- server/db/db.js
+++ server/db/db.js
@@ -14,11 +14,10 @@
 import mongoose from "mongoose";
 
 const connectToDb = () => {
-  mongoose
-    .connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
+ mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
     .then(() => {
       console.log("Connected to database");
     })
